e3cfe43b91eea07520611a667dd8ba483ea857c2
breyllad <105427091+breyllad@users.noreply.github.com>
2023-10-10 11:15:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function tocaSomPom (){
   document.querySelector('#som_tecla_pom).play();
   }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
-fuction tocaSompom () {
+fuction tocaSomClap () {
   document. querySelector('.tecla_clap').play();
 }
-document.querySelector(('.tecla_clap').onclick = tocaSomClap;
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
